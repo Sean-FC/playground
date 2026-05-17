98a221ec07eabf1c65cc80edee6a4626aaa02e30
SeanC <69810844+Sean-FC@users.noreply.github.com>
2026-05-17 20:55:18 +0200

    chore: simplify usage of k3s enabled toggles (#41)

--- infra/aws/crowdsec.tf
+++ infra/aws/crowdsec.tf
@@ -1,7 +1,6 @@
 resource "aws_ssm_parameter" "crowdsec" {
-  count = var.k3s_enabled ? 1 : 0
-  name  = "/${module.context.stage}/eso/crowdsec"
-  type  = "SecureString"
+  name = "/${module.context.stage}/eso/crowdsec"
+  type = "SecureString"
   value = jsonencode({
     csLapiSecret      = local.secrets_main.crowdsec.cs_lapi_secret
     registrationToken = local.secrets_main.crowdsec.registration_token
